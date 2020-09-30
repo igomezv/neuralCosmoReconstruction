928da095fde7e2aaf99e7ad55b6e8a7629124483
igomezv <neorelativista@gmail.com>
2020-09-29 23:11:30 -0500

    AutoEncoder class

---
+++ AutoEncoder.py
@@ -0,0 +1,95 @@
+import tensorflow.keras
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.initializers import Constant, RandomNormal
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from matplotlib import pyplot as plt
+
+class NeuralNet:
+    def __init__(self, X, y, **kwargs):
+        try:
+            _, self.input_nodes = np.shape(X)
+        except:
+            self.input_nodes = 1
+        try:
+            _, self.output_nodes = np.shape(y)
+        except:
+            self.output_nodes = 1
+        self.initializer = RandomNormal()
+        split = kwargs.pop('split', 0.8)
+        self.hidden_nodes_1 = kwargs.pop('hidden_nodes_2', 600)
+        self.hidden_nodes_2 = kwargs.pop('hidden_nodes_2', 300)
+        self.hidden_nodes_3 = kwargs.pop('hidden_nodes_3', 300)
+        self.last_nodes = kwargs.pop('last_nodes', self.output_nodes)
+        self.batch_size = kwargs.pop('batch_size', 64)
+        self.epochs = kwargs.pop('epochs', 500)
+        min_delta = kwargs.pop('min_delta', 0)
+        patience = kwargs.pop('patience', 10)
+
+        randomize = np.random.permutation(len(X))
+        X = X[randomize]
+        y = y[randomize]
+        ntrain = int(split * len(X))
+        indx = [ntrain]
+        self.X_train, self.X_test = np.split(X, indx)
+        self.y_train, self.y_test = np.split(y, indx)
+
+        input_x = Input(shape=(self.input_nodes,))
+        self.model = Model(input_x, self.autoencoder(input_x))
+        self.model.compile(loss='mean_squared_error', optimizer="adam")
+        self.model.summary()
+        self.callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min',
+                                                           min_delta=min_delta,
+                                                           patience=patience,
+                                                           restore_best_weights=True)]
+        self.trained_model = self.fit()
+
+    def autoencoder(self, input_cov):
+        # encoder
+        encoded1 = Dense(600, activation='relu')(input_cov)
+        encoded2 = Dense(300, activation='relu')(encoded1)
+        encoded3 = Dense(100, activation='relu')(encoded2)
+        coded = Dense(1, activation='relu', )(encoded3)
+        decoded1 = Dense(100, activation='relu')(coded)
+        decoded2 = Dense(300, activation='relu')(decoded1)
+        decoded3 = Dense(600, activation='relu')(decoded2)
+        decoded_out = Dense(1048, activation='relu')(decoded3)
+
+        return decoded_out
+
+    # input_cov = Input(shape=(1048,))
+
+    def fit(self):
+        return self.model.fit(self.X_train, self.y_train,
+                                    batch_size=self.batch_size,
+                                    epochs=self.epochs, verbose=1,
+                                    validation_data=(self.X_test, self.y_test),
+                                    callbacks=self.callbacks)
+
+    def predict(self, _X, model=None):
+        if model:
+            return model.predict(_X)
+        else:
+            return self.model.predict(_X)
+
+    def plot(self, **kwargs):
+        outputname = kwargs.pop('outputname', 'loss_AE')
+        train_color = kwargs.pop('train_color', 'r')
+        val_color = kwargs.pop('val_color', 'g')
+        show = kwargs.pop('show', False)
+        title = kwargs.pop('title', 'AE loss function')
+
+        plt.plot(self.trained_model.history['loss'], color=train_color)
+        plt.plot(self.trained_model.history['val_loss'], color=val_color)
+        plt.title(title)
+        plt.ylabel('loss function')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+        plt.savefig('{}.png'.format(outputname))
+        if show:
+            plt.show()
+
+
+
+
