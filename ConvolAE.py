0586405dfde173f912867beb9fe9cadd7bf74e96
igomezv <neorelativista@gmail.com>
2020-10-16 09:27:47 -0500

    update convolae

--- ConvolAE.py
+++ ConvolAE.py
@@ -4,26 +4,39 @@ from tensorflow.keras import layers
 from tensorflow.keras import Input
 
 class ConvolAE(NeuralNet):
-    def __init__(self, X, **kwargs):
-        super(ConvolAE, self).__init__(self, X, y=X, **kwargs)
+    def __init__(self, X, y, **kwargs):
+        split = kwargs.pop('split', 0.8)
+        self.batch_size = kwargs.pop('batch_size', 64)
+        self.epochs = kwargs.pop('epochs', 500)
+        min_delta = kwargs.pop('min_delta', 0)
+        patience = kwargs.pop('patience', 10)
+        self.lr = kwargs.pop('lr', 0.0001)
+        super(ConvolAE, self).__init__(X, y, **kwargs)
 
     def model(self):
         input_cov = Input(shape=(1048, 1048, 1))
-        x = layers.Conv2D(10, (50, 50), activation='relu')(input_cov)
-        # shape_1 = [1048 - (n-1)] x [1048 - (n-1)]
-        x = layers.MaxPooling2D((4, 4))(x)
-        # shape_2 = [(shape_1[0] -1)/2]
-        x = layers.Flatten()(x)
-        x = layers.Dense(1048)(x)
-        # autoencoder.add(Dense(10))
+        # input_cov = Input(shape=(1048,))
+        input_cov = Input(shape=(1048, 1048, 1))
+        # x = layers.Conv2D(1, (50, 50), activation='relu', padding='same')(input_cov)
+        # # shape_1 = [1048 - (n-1)] x [1048 - (n-1)]
+        # x = layers.MaxPooling2D((3, 3))(x)
+        # # shape_2 = [(shape_1[0] -1)/2]
+        x = Conv2D(2, (3, 3), activation='relu', padding='same')(input_cov)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(1, (4, 4), activation='relu', padding='same')(x)
+        encoded = MaxPooling2D((4, 4), padding='same')(x)
 
-        # x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-        # x = layers.MaxPooling2D((2, 2), padding='same')(x)
-        # x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-        # x = layers.MaxPooling2D((2, 2), padding='same')(x)
-        # x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-        # encoded = layers.MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(1, (4, 4), activation='relu', padding='same')(encoded)
+        x = UpSampling2D((4, 4))(x)
+        x = Conv2D(2, (3, 3), activation='relu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+        # x = layers.Flatten()(x)
+        # encoded = layers.Dense(1048)(x)
 
         autoencoder = tf.keras.Model(input_cov, x)
-        autoencoder.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+        autoencoder.compile(optimizer='adam', loss='mean_squared_error')
         autoencoder.summary()
+
+        return autoencoder
