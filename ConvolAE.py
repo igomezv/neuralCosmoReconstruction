ded3dd939297deee5abead482089c77ff836943f
igomezv <neorelativista@gmail.com>
2020-10-16 12:45:01 -0500

    changing conv arch

--- ConvolAE.py
+++ ConvolAE.py
@@ -1,6 +1,6 @@
 from NeuralNet import NeuralNet
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras.layers import Conv2D, Dense, UpSampling2D, MaxPooling2D
 from tensorflow.keras import Input
 
 class ConvolAE(NeuralNet):
@@ -15,28 +15,26 @@ class ConvolAE(NeuralNet):
 
     def model(self):
         input_cov = Input(shape=(1048, 1048, 1))
-        # input_cov = Input(shape=(1048,))
-        input_cov = Input(shape=(1048, 1048, 1))
         # x = layers.Conv2D(1, (50, 50), activation='relu', padding='same')(input_cov)
         # # shape_1 = [1048 - (n-1)] x [1048 - (n-1)]
         # x = layers.MaxPooling2D((3, 3))(x)
         # # shape_2 = [(shape_1[0] -1)/2]
-        x = Conv2D(2, (3, 3), activation='relu', padding='same')(input_cov)
+        x = Conv2D(6, (3, 3), activation='relu', padding='same')(input_cov)
         x = MaxPooling2D((2, 2), padding='same')(x)
-        x = Conv2D(1, (4, 4), activation='relu', padding='same')(x)
+        x = Conv2D(4, (4, 4), activation='relu', padding='same')(x)
         encoded = MaxPooling2D((4, 4), padding='same')(x)
 
-        x = Conv2D(1, (4, 4), activation='relu', padding='same')(encoded)
+        x = Conv2D(4, (4, 4), activation='relu', padding='same')(encoded)
         x = UpSampling2D((4, 4))(x)
-        x = Conv2D(2, (3, 3), activation='relu', padding='same')(x)
+        x = Conv2D(6, (3, 3), activation='relu', padding='same')(x)
         x = UpSampling2D((2, 2))(x)
         decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
         # x = layers.Flatten()(x)
         # encoded = layers.Dense(1048)(x)
 
-        autoencoder = tf.keras.Model(input_cov, x)
-        autoencoder.compile(optimizer='adam', loss='mean_squared_error')
+        autoencoder = tf.keras.Model(input_cov, decoded)
+        autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
         autoencoder.summary()
 
         return autoencoder
